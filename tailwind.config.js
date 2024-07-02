aab6a048e874f10a77e0d3012641db3b07ead14c
Meet Bhesaniya <105696370+bhemi28@users.noreply.github.com>
2024-07-02 15:49:42 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  './**/*.{html,js}',
+  './**/*.{html,js}',
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
+
